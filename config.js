9111d1f021d1cc1201a6b65fa410598547f6c97e
brucefengnju <brucechenofnju@gmail.com>
2013-01-27 14:53:15 +0800

    first commit
    Signed-off-by: brucefengnju <brucechenofnju@gmail.com>

---
+++ config.js
@@ -0,0 +1,14 @@
+/**
+ * config
+ */
+
+exports.config = {
+  debug: true,
+  name: 'sinter',
+  description: 'this project was written in winter',
+  version: '0.0.1',
+  site_headers: [
+    '<meta name="author" content="brucefeng" />',
+  ],
+  db: 'mongodb://127.0.0.1/sinter_db'
+};
