21d86811c8f5d42a856fd2d8e67537408d3d1875
brucefengnju <brucechenofnju@gmail.com>
2013-01-27 16:00:18 +0800

    add index controller
    Signed-off-by: brucefengnju <brucechenofnju@gmail.com>

---
+++ controllers/site.js
@@ -0,0 +1,10 @@
+/*!
+ * sinter - site index controller.
+ * Copyright(c) 2013 brucefeng <brucechenofnju@gmail.com>
+ * MIT Licensed
+ */
+
+var config = require('../config').config;
+
+exports.index = function (req, res, next) {
+};
