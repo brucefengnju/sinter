f9513b301d366cd01e5ecf32b13000564ee02dec
brucefengnju <brucechenofnju@gmail.com>
2013-03-26 21:35:46 +0800

    add savename controller

--- controllers/sign.js
+++ controllers/sign.js
@@ -8,56 +8,56 @@ var mongoose = require('mongoose');
 var config = require('../config').config;
 var model = require('../models');
 var User = model.User
+var userProxy = require('../proxy/userproxy');
+
 exports.login = function (req,res,next) {
-	if(typeof req.body.assertion === 'undefined' || req.body.assertion === null ||req.body.assertion ===''){
-		return res.redirect('index');
-	}
-	
-	var options = {
-		hostname:'verifier.login.persona.org',
-		path:'/verify',
-		method:'POST'
-	};
+    if(typeof req.body.assertion === 'undefined' || req.body.assertion === null ||req.body.assertion ===''){
+        return res.redirect('index');
+    }
+    
+    var options = {
+        hostname:'verifier.login.persona.org',
+        path:'/verify',
+        method:'POST'
+    };
 
-	var vreq = https.request(options,function(vres){
-		var body = '';
-		vres.on('error',function (error) {
-			console.log(error);
-		});
-		vres.on('data',function (chunk) {
-			body += chunk;
-		});
-		vres.on('end',function(){
-			try{
-				var response = JSON.parse(body),
-              	valid = response && response.status === "okay";
-              	if(valid){
-              		req.session['email'] = response.email;
-              		User.findOne({'email':response.email},function (err,user) {
-              			if(err){
-              				next(err);
-              			}
-              			if(!user){
-              				res.locals.userName= null;
-              			}else{
-              				res.locals.userName = user.name;
-              			}
-              		});
-              	}else{
-              		req.session['email'] = null;
-              	}
-              	//res.json(response);
-              	res.json({name:res.locals.userName,email:response.email,status:response.status});
+    var vreq = https.request(options,function(vres){
+        var body = '';
+        vres.on('error',function (error) {
+            console.log(error);
+        });
+        vres.on('data',function (chunk) {
+            body += chunk;
+        });
+        vres.on('end',function(){
+            try{
+                var response = JSON.parse(body),
+                valid = response && response.status === "okay";
+                if(valid){
+                    req.session['email'] = response.email;
+                    userProxy.getUserByName(response.email,function(err,user){
+                        if(err){
+                            next(err);
+                        }
+                        if(user){
+                            return res.json({'login':true,'needname':false,'email':user.email,'name':user.name});
+                        }else{
+                            return res.json({'login':true,'needname':true,'email':response.email,'name':''});
+                        }
+                    });
+                }else{
+                    req.session['email'] = null;
+                    return res.json({'login':true});
+                }
             }catch(e){
-            	console.log(e);
+                console.log(e);
             }
-
-		});
-	});
-	vreq.on('error',function(error){
-		console.log(error);
-	});
-	vreq.setHeader("Content-Type", "application/json");
+        });
+    });
+    vreq.on('error',function(error){
+        console.log(error);
+    });
+    vreq.setHeader("Content-Type", "application/json");
     var data = JSON.stringify({
       assertion: req.body.assertion,
       audience: 'http://127.0.0.1:8080'
@@ -65,11 +65,22 @@ exports.login = function (req,res,next) {
     vreq.setHeader("Content-Length", data.length);
     vreq.end(data);
 }
+exports.saveName = function(req,res,next){
+    if(!req.body.name || !req.body.email){
+        return res.json({'success':false});
+    }
+    var name = req.body.name;
+    var email = req.body.email;
+    userProxy.saveOrUpdateUser(name,email,function(err){
+        if(err){
+            res.json(500,{'error':err});
+        }
+        return res.json({'success':true,'email':email,'name':name});
+    });
+}
 exports.logout = function(req,res){
-	console.log('log out');
-	if(req.session['email'] !== null && req.session['email'] !== undefined){
-		req.session['email'] = null;
-	}
-	res.json({status:'okay'})
-	res.redirect('index');
+    if(req.session['email'] !== null && req.session['email'] !== undefined){
+        req.session['email'] = null;
+    }
+    res.json({status:'okay'})
 }
