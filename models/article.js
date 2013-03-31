544526fe6d26f1f2a383497c652d52cdbc7bc1f6
brucefengnju <brucechenofnju@gmail.com>
2013-03-31 17:33:53 +0800

    update article

--- models/article.js
+++ models/article.js
@@ -10,7 +10,7 @@ var ArticleSchema = new Schema({
     article_id:{type:String,index:true},
     title:{type:String},
     content:{type:String},
-    author:{ type: Schema.Types.ObjectId,ref:'users'},
+    author:{ type: Schema.Types.ObjectId,ref:'User'},
     publishTime:{type:Date,default:Date.now},
     draft:{type:Boolean,default:false}
 });
