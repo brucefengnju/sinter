3aaee396bf9263a0cb8071a3268cb6c969d5f274
brucefengnju <brucechenofnju@gmail.com>
2013-03-31 17:34:44 +0800

    add population

--- proxy/articleproxy.js
+++ proxy/articleproxy.js
@@ -10,25 +10,25 @@ exports.getArticlesByTitle = function(title,callback){
     if(!title || title.length ===0){
         return callback(null,[]);
     }
-    Article.find({'title':title},callback);
+    Article.find({'title':title}).populate('author').exec(callback);
 }
 exports.getArticleById = function(id,callback){
     if(!id || !id.length){
         return callback(null,[]);
     }
-    Article.findOne({'_id':id},callback);
+    Article.findOne({'_id':id}).populate('author').exec(callback);
 }
 exports.getArticlesByAuthor = function(author,callback){
     if(!author){
         return callback(null,[]);
     }
-    Article.find({'author':author},callback);
+    Article.find({'author':author}).populate('author').exec(callback);
 }
 exports.getArticle = function(author,title,callback){
     if(!author || !title){
         return callback(null,[]);
     }
-    Article.findOne({'author':author,'title':title},callback);
+    Article.findOne({'author':author,'title':title}).populate('author').exec(callback);
 }
 exports.updateArticle = function(article,callback){
     if(article === null){
